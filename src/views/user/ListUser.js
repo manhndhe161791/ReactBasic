fad718540cd96db6c4bc76064d909216eba03a52
manhndhe161791 <manhndhe161791@fpt.edu.vn>
2023-03-20 15:23:45 +0700

    Route Parameter

--- src/views/user/ListUser.js
+++ src/views/user/ListUser.js
@@ -1,15 +1,52 @@
 import React from "react";
 import axios from "axios";
+import './ListUser.scss'
+import { withRouter } from "react-router-dom";
+
 class ListUser extends React.Component {
 
-    componentDidMount() {
-
+    state = {
+        listUser: []
     }
+    // componentDidMount() {
+    //     axios.get('https://reqres.in/api/users?page=2').then(res => {
+    //         console.log('>>> check response', res.data.data)
+    //     })
+    // }
+    async componentDidMount() {
+        let res = await axios.get('https://reqres.in/api/users?page=1');
+        this.setState({
+            listUser: res && res.data && res.data.data ? res.data.data : []
+        })
+    }
+
+    handleViewUserDetail = (user) => {
+        this.props.history.push(`/user/${user.id}`)
+    }
+
     render() {
+        let { listUser } = this.state;
         return (
-            <div> List user</div>
+            <>
+                <div className="list-user-container">
+                    List user
+                    <div className="list-user-content">
+                        {listUser && listUser.length > 0 &&
+                            listUser.map((item, index) => {
+                                return (
+                                    <div className="child" key={item.id}
+                                        onClick={() => this.handleViewUserDetail(item)}
+                                    >
+                                        {index + 1} - {item.first_name} {item.last_name}
+                                    </div>
+                                )
+                            })
+                        }
+                    </div>
+                </div>
+            </>
         )
     }
 }
 
-export default ListUser;
+export default withRouter(ListUser);
